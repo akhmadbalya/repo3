65780a55527a51f41048c376a9c6cf1eef958281
fitri15 <f.ya15@yahoo.co.id>
2013-10-04 14:51:39 +0700

    praktikum3

---
+++ praktikum3/New folder/latihan.java
@@ -0,0 +1,16 @@
+latihan tono = new latihan();
+
+public void iniMethod(){
+}
+
+publicString getNama(){
+	return "dani";
+}
+
+private void methodParam(String nama){
+}
+
+private int tambah(int a, int b){
+	return a+b;
+	}
+	
